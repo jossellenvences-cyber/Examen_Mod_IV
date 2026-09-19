f483be3b9d5f71d57b7a54de7fc3d7084dfb07e5
jossellenvences-cyber <jossellenvences@gmail.com>
2026-09-19 01:51:58 -0600

    Nobel.py

--- Nobel.py
+++ Nobel.py
@@ -1,7 +1,7 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.tree import DecisionTreeClassifier
 
 
