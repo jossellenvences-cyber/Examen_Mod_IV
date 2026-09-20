77a7190ff378d4da317e51e28504837a2d7b094a
jossellenvences-cyber <jossellenvences@gmail.com>
2026-09-19 21:59:42 -0600

    Nobel.py

--- Nobel.py
+++ Nobel.py
@@ -1,8 +1,11 @@
+%%writefile Nobel.py
 import numpy as np
 import streamlit as st
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.tree import DecisionTreeClassifier
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
 
 
 st.write(''' # Premios Nobel ''')
@@ -26,32 +29,43 @@ nobel =  pd.read_csv('df_nobel_final.csv', encoding='latin-1')
 x = nobel.Text
 y = nobel.Label
 
-vect = CountVectorizer()
-x_dtm = vect.fit_transform(x)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, random_state=42)
 
-nb = DecisionTreeClassifier()
-nb.fit(x_dtm, y)
+for k in k_list:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    modelo_knn = knn.fit(x_train, y_train)
+
+    y_pred = modelo_knn.predict(x_test)
+  
+
+    mse = mean_squared_error(y_test, y_pred)
+    
+    knn_dict[k] = mse
+    
+
+#vect = CountVectorizer()
+#x_dtm = vect.fit_transform(x)
+
+#nb = MultinomialNB()
+#nb.fit(x_dtm, y)
 
 #df_dtm = vect.transform(df['Text'])
+
 #prediction = nb.predict(df_dtm)
 
-classifier = DecisionTreeClassifier(max_depth=8, criterion='texto_limpio', min_samples_leaf=10, max_features=7, random_state=0)
-classifier.fit(x, y)
-
-prediction = classifier.predict(df)
 
 st.subheader('Predicción')
-if prediction == 0:
+if y_pred == 0:
   st.write('Physics')
-elif prediction == 1:
+elif y_pred == 1:
   st.write('Medicine')
-elif prediction == 2:
+elif y_pred == 2:
   st.write('Peace')
-elif prediction == 3:
+elif y_pred == 3:
   st.write('Literature')
-elif prediction == 4:
+elif y_pred == 4:
   st.write('Chemistry')
-elif prediction == 5:
+elif y_pred == 5:
   st.write('Economics')
 else:
   st.write('Sin Predicción')
